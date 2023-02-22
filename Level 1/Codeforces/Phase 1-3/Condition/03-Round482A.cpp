d8a94fb5e3bc9f625187564e6668af06fa265da2
Shadi Bahaa <33010011+ShadiBahaa@users.noreply.github.com>
2023-02-22 17:53:52 +0200

    Create 03-Round482A.cpp

---
+++ Level 1/Codeforces/Phase 1-3/Condition/03-Round482A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    if (n==0){
+        cout<<0<<endl;
+        return 0;
+    }
+    if (n%2){
+        cout<<(n+1)/2<<endl;
+    }else {
+        cout<<n+1<<endl;
+    }
+}
